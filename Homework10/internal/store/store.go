89b082d6f1c596186c6b80f81c317bc15d41f2a0
Ayana Adilova <aadilova19@gmail.com>
2021-11-28 11:39:33 +0600

    commit

---
+++ Homework10/internal/store/store.go
@@ -0,0 +1,30 @@
+package store
+
+import (
+	"context"
+	"hw10/internal/models"
+)
+
+type Store interface {
+	Connect(url string) error
+	Close() error
+
+	Categories() CategoriesRepository
+	Accesories() AccesoriesRepository
+}
+
+type CategoriesRepository interface {
+	Create(ctx context.Context, category *models.Category) error
+	All(ctx context.Context) ([]*models.Category, error)
+	ByID(ctx context.Context, id int) (*models.Category, error)
+	Update(ctx context.Context, category *models.Category) error
+	Delete(ctx context.Context, id int) error
+}
+
+type AccesoriesRepository interface {
+	Create(ctx context.Context, good *models.Accesory) error
+	All(ctx context.Context) ([]*models.Accesory, error)
+	ByID(ctx context.Context, id int) (*models.Accesory, error)
+	Update(ctx context.Context, good *models.Accesory) error
+	Delete(ctx context.Context, id int) error
+}
